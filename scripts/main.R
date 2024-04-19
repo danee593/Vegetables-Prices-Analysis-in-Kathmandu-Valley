0113a65c02bb989815fe913c8edd16ad4118e744
Grumpy Gonzalez <emmagcasalini@gmail.com>
2024-04-19 18:04:53 +0100

    Update main.R
    Tidy up cpi

--- scripts/main.R
+++ scripts/main.R
@@ -1,5 +1,5 @@
-install.packages("lmtest")
 # Required Libraries
+install.packages("lmtest")
 library(readr)
 library(tidyverse)
 library(fpp3)
@@ -14,6 +14,17 @@ theme_set(theme_bw())
 veggies <- read_csv("data_sources/kalimati_tarkari_dataset.csv", 
                     col_types = cols(Date = col_date(format = "%Y-%m-%d")))
 
+cpi <- read_excel("data_sources/Consumer Price Index, All items.xlsx", 
+                 skip = 7)
+
+# tidy up cpi
+base_index <- cpi$Index[1]
+
+cpi <- cpi %>% 
+  mutate(date = yearmonth(cpi$Units),
+         Index = Index/base_index) %>% 
+  select(c(date, Index))
+
 # Get only 5 keys and 1 variable.
 veggies <- veggies %>% 
   filter(Unit == "Kg") %>% 
@@ -22,88 +33,121 @@ veggies <- veggies %>%
                           "Cabbage(Local)")) %>%
   select(Commodity, Date, Average)
 
-# Convert to tsibble
-veggies_ts <- veggies %>% 
-  as_tsibble(key = Commodity,
-             index = Date)
 
-# Plot timeline of average price per commodity
-veggies_ts %>% 
-  autoplot()
-
-# Start by looking at the Cabbage (Month is the index and Commodity is the key)
-Cabbage <- veggies_ts %>%
+Cabbage <- veggies %>% 
   filter(Commodity == "Cabbage(Local)")
 
-# Fill the gaps & missing values (otherwise decomposition do not work)
-Cabbage_filled <- Cabbage_filled %>%
-  fill(Average)
 
-Cabbage_filled %>% autoplot(Average)
+cabbage_constant_prices <- Cabbage |>
+  mutate(ym = yearmonth(Date)) %>% 
+  inner_join(cpi, by = join_by(ym == date)) %>% 
+  mutate(Average_constant = Average / Index) %>% 
+  select(Date, Commodity, Average_constant)
+
+Cabbage_tsibble <- as_tsibble(cabbage_constant_prices, key = Commodity, index = Date)
+
+CabbageMonthly <- Cabbage_tsibble %>%
+  index_by(Month = yearmonth(Date)) %>%
+  summarise(Average = mean(Average_constant))
+
+# Looks good (aka it turned into monthly)
+print(CabbageMonthly)
+
+CabbageMonthly %>% autoplot(Average)
 # Looking at the plot we can see that there is a slightly positive trend and seasonality going on.
 # looking at it, it looks like the cabbage is more expensive in the beginning of the year (possibly due to weather/sparsity.)
 # We can also see that this trend is additive rather than multiplicative, suggesting that we might not need a mathematical transformation)
 # Box-cox is used to normalize skewed data and get heterogeneity, we can check how heterogenic the data is to decide whether or not a box-cox is a good choice.
 
-Cabbage_filled %>% ACF(Average) %>% autoplot()
+CabbageMonthly %>% ACF(Average) %>% autoplot()
 
-# Looking at the ACF we see a positive autocorrelation that decreases over time.It is hard to see any seasonality by this plot.
-# But we see that this data is trended because the ACF of a trended time series tends to have positive values that slowly decrease as the lags increase.
+# Looking at the ACF we see a positive autocorrelation as well as negative correlation. 
+# The positive correlation is for the two first lags and the negative is mainly for lag 5,6 and 7.
+# positive autocorrelation at the early lags suggests persistence in the price behavior, 
+# whilst the negative autocorrelation at  lag 5,6, and 7 lags could suggest some sort of periodic behavior or seasonality in the data.
 
 # Now we can check if a box-cox would do any difference, we can first see if there is any heteroskedasticity
-# the breusch-pagan test suggests that there is evidence of heteroscedasticity
-lm_model <- lm(Average ~ Date, data = Cabbage_filled)
+# the breusch-pagan test suggests that there is no need for boxcox (0.723) we do not reject hypothesis that we have homoskedasticity
+lm_model <- lm(Average ~ Month, data = CabbageMonthly)
 bptest(lm_model)
 
-# Box-cox transformation - let´s check with and without box-cox
-Cabbage_filled %>%
+# Box-cox transformation - let´s check with and without box-cox anyways (for fun and science)
+# But as we see, maybe it accounts for some variance, but not really..there is no siginifcant differnce
+CabbageMonthly%>%
   features(Average, features = guerrero) %>%
   pull(lambda_guerrero)-> lambda
 
-Cabbage_filled %>% autoplot(Average)
+CabbageMonthly%>% autoplot(Average)
 
-Cabbage_filled %>%
+CabbageMonthly%>%
   autoplot(box_cox(Average, lambda))
 
-Cabbage_filled <- Cabbage_filled %>%
-  mutate(Average_transformed = box_cox(Average, lambda))
-
-# So we can see that the box-cox transformation actually did account for some variance.
-# stabilize the variance of data. It achieveshttp://127.0.0.1:46763/graphics/eae0aabb-fca1-42fc-af17-adb617189384.png this by applying a power transformation to the data, 
-# effectively adjusting the data to make its variance more constant, when we apply the Box-Cox transformation 
-# to the cabbage data, it becomes more homoscedastic.
-
 # Decomposition let's try different decomposition models and assess which one is the best.
-# X11 only works on monthly or quarterly data - so those we can forget about - SEATS does not work either
-Cabbage_filled %>%
+# Looking at the decomposition models we can see in the plots that the x11 (X_13ARIMA_SEATS) 
+# is the best decomposition model
+
+CabbageMonthly %>%
   model(STL1 = STL(Average),
         STL2 = STL(Average ~ trend(window = 7) + season(window = 5), robust = TRUE),
-        fixed = STL(Average ~ season(window = "periodic"), robust = TRUE))
-
-  components() -> dcmp
+        fixed = STL(Average ~ season(window = "periodic"), robust = TRUE),
+        x11 = X_13ARIMA_SEATS(Average),
+        seats = X_13ARIMA_SEATS(Average ~ seats()))%>%
+  components() -> dcmpCabbage
   
-dcmp %>% as_tsibble() %>% autoplot(trend)
-dcmp %>% as_tsibble() %>% autoplot(season_adjust)
+dcmpCabbage %>% as_tsibble() %>% autoplot(trend)
+dcmpCabbage %>% as_tsibble() %>% autoplot(season_adjust)
+autoplot(dcmpCabbage)
+
+# Create training data/set and checking which method model is best for the cabbage time series
+nrow(CabbageMonthly)
+
+# we can train on 25 rows
+CabbageMonthly %>% slice(1:(n()-40))-> train_cabbage
+
+Cabbagefit <- train_cabbage%>%
+  model(
+    mean = MEAN(Average),
+    Naive = NAIVE(Average),
+    Seasonal_Naive = SNAIVE(Average),
+    Drift = RW(Average ~ drift())
+  ) 
+
+CabbageFC<- Cabbagefit %>%
+  forecast(h = 5) %>%
+  autoplot(train_cabbage, level = NULL) +
+  labs(title = "Forecasts for monthly cabbage price") +
+  guides(color = guide_legend(title = "Forecast"))
+
+CabbageFC
+# The best method model for is the random walk with a drift according to the accuracy (RMSE)
+# Residual Diagnostics
+# When looking at the residuals they resemble white noise, which indicates a good method model.
+# The innovation residuals are uncorrelated. If there are correlations between innovation residuals, 
+# then there is information left in the residuals which should be used in computing forecasts.
+# The innovation residuals have zero mean. If they have a mean other than zero, then the forecasts are biased.
+# Basically, we want our residuals to be white noise, normally distributed and no variance. This is a sign that the model is good
+# That RW is the best model here might not come as a surprise, as that model usually do good when we have a positive trend in our data.
+# the naive model here would also have been good but mean and snaive is not good because their residuals are not white noise.
+
+accuracy(Cabbagefit)
+Cabbagefit %>% accuracy() %>% arrange(RMSE)
+
+CabbageDrift <- train_cabbage %>%
+  model(RW(Average ~ drift()))
+
+CabbageDrift %>%
+  gg_tsresiduals() 
+
+CabbageNaive <- train_cabbage %>%
+  model(NAIVE(Average))
+
+CabbageNaive %>%
+  gg_tsresiduals() 
+
+# Exponential smoothing
 
 
-dcmp %>% 
-  autoplot(trend)
 
-dcmp %>% 
-  autoplot(season_year)
-
-autoplot(dcmp)
-
-# Checking which method is best for the cabbage time series
-fitCabbage <- Cabbage_filled %>% 
-  model(STL2 = STL(Average ~ trend(window = 7) + season(window = 5), robust = TRUE),
-        naive = NAIVE(Average),
-        snaive = SNAIVE(Average),
-        mean = MEAN(Average),
-        RW = RW(Average ~ drift()))
-
-fitCabbage %>%
-  augment() %>% ACF(.resid) %>% autoplot()
 
 
 
